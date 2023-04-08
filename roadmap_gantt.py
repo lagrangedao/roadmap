4cccfa871f97c160b5992b3ffff10745fda231ed
Charles Cao <nebulablock@gmail.com>
2023-04-08 11:21:11 -0400

    update milestone

--- roadmap_gantt.py
+++ roadmap_gantt.py
@@ -1,14 +1,20 @@
 import os
+import plotly.express as px
+import pandas as pd
 
 import plotly.figure_factory as ff
 
-df = [dict(Task="Milestone Setup", Start='2022-11-01', Finish='2022-12-31', Complete=100),
-      dict(Task="Dataset", Start='2023-01-01', Finish='2023-03-01', Complete=80),
-      dict(Task="Space", Start='2023-02-01', Finish='2023-04-01', Complete=60),
-      dict(Task="Computing provider", Start='2023-03-01', Finish='2023-04-15', Complete=30)]
+df = pd.DataFrame([
+    dict(Task="Milestone Setup", Start='2022-11-01', Finish='2022-12-31', Resource="MileStone 1"),
+    dict(Task="Dataset", Start='2023-01-01', Finish='2023-03-01', Resource="MileStone 2"),
+    dict(Task="Space", Start='2023-02-01', Finish='2023-04-01', Resource="MileStone 2"),
+    dict(Task="Computing provider", Start='2023-03-01', Finish='2023-04-15', Resource="MileStone 3")
+])
 
-fig = ff.create_gantt(df, title="lagrange DAO Roadmap", colors='Viridis', index_col='Complete', show_colorbar=True)
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Resource")
+
 # fig.show()
 if not os.path.exists("images"):
     os.mkdir("images")
 fig.write_image("images/roadmap.png")
+fig.show()
