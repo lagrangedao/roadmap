94f029dd1a3f3aaca1e077e5bfcd8af0e7a0630f
Charles Cao <nebulablock@gmail.com>
2023-04-08 21:44:30 -0400

    update milestone name in image

--- roadmap_gantt.py
+++ roadmap_gantt.py
@@ -5,21 +5,20 @@ import pandas as pd
 import plotly.figure_factory as ff
 
 df = pd.DataFrame([
-    dict(Task="Milestone Setup", Start='2022-11-01', Finish='2022-12-31', Resource="M1"),
-    dict(Task="Dataset", Start='2023-01-01', Finish='2023-03-01', Resource="M2"),
-    dict(Task="Space", Start='2023-02-01', Finish='2023-04-01', Resource="M2"),
-    dict(Task="Computing provider", Start='2023-03-01', Finish='2023-04-15', Resource="M3")
+    dict(Component="Milestone Setup", Start='2022-11-01', Finish='2022-12-31', Milestone="M1"),
+    dict(Component="Dataset", Start='2023-01-01', Finish='2023-03-01', Milestone="M2"),
+    dict(Component="Space", Start='2023-02-01', Finish='2023-04-01', Milestone="M2"),
+    dict(Component="Computing provider", Start='2023-03-01', Finish='2023-04-15', Milestone="M3")
 ])
 
-fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color='Resource')
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Component", color='Milestone')
 fig.update_layout(
     font_family="Arial",
     title_font_family="Arial",
     title_font_color="black",
     legend_title_font_color="black"
 )
-# fig.show()
 if not os.path.exists("images"):
     os.mkdir("images")
 fig.write_image("images/roadmap.svg", engine='kaleido',width=800, height=400)
-fig.show()
+# fig.show()
